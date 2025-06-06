6e5688f5c1bfaa52a1a6b2454fbd37bea8eacfdf
Yonatan Amir <jonathanamir@gmail.com>
2025-06-06 11:42:31 +0200

    added foundation tests for track_content_model

--- backend/app/models/track_content.rb
+++ backend/app/models/track_content.rb
@@ -1,4 +1,11 @@
 class TrackContent < ApplicationRecord
   belongs_to :container
   belongs_to :user
+  
+  # Add this line:
+  has_many :file_attachments, as: :attachable, dependent: :destroy
+  
+  validates :title, presence: true
+  validates :user, presence: true
+  validates :container, presence: true
 end
